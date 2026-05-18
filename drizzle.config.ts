a3b03c4cd9f7bffe621237fe8b6b17a7481347b9
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-18 18:33:14 -0300

    chore(project): configure initial backend project

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'drizzle-kit';
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is required');
+}
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/database/schemas/*.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: databaseUrl,
+  },
+});
