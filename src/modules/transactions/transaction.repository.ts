ed9742c282ac745c72dcfa0709701da1dbce5927
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-05-23 11:34:50 -0300

    feat(transactions): add DTO schemas and repository

---
+++ src/modules/transactions/transaction.repository.ts
@@ -0,0 +1,35 @@
+import { eq } from 'drizzle-orm'
+import type { Database } from '../../database/db.js'
+import {
+  type NewTransaction,
+  type Transaction,
+  transactionsTable,
+} from '../../database/schemas/transactions.schema.js'
+
+export class TransactionRepository {
+  constructor(private readonly database: Database) {}
+
+  async create(data: NewTransaction): Promise<Transaction | null> {
+    const [transaction] = await this.database
+      .insert(transactionsTable)
+      .values(data)
+      .returning()
+
+    return transaction ?? null
+  }
+
+  async findById(id: string): Promise<Transaction | null> {
+    const [transactions] = await this.database
+      .select()
+      .from(transactionsTable)
+      .where(eq(transactionsTable.id, id))
+
+    return transactions ?? null
+  }
+
+  async delete(id: string) {
+    await this.database
+      .delete(transactionsTable)
+      .where(eq(transactionsTable.id, id))
+  }
+}
