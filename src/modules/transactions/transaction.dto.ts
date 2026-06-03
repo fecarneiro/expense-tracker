bfa9e58a6299d4509abd23e8a65c3c87e4bd233a
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-06-03 07:21:45 -0300

    feat(transactions): add pagination support to findAll method and update related schemas

--- src/modules/transactions/transaction.dto.ts
+++ src/modules/transactions/transaction.dto.ts
@@ -18,10 +18,19 @@ const transactionBaseSchema = z.object({
 
 // HTTP schemas
 export const transactionIdParamsSchema = z.object({ id: z.uuid() })
+
+export const transactionQueryParamsSchema = z.object({
+  limit: z.coerce.number().int().min(1).max(100).default(20),
+  offset: z.coerce.number().int().min(0).default(0),
+})
+
 export const createTransactionSchema = transactionBaseSchema
+
 export const updateTransactionSchema = transactionBaseSchema
   .partial()
-  .refine((data) => Object.keys(data).length > 0)
+  .refine((data) => Object.keys(data).length > 0, {
+    message: 'Inform at least 1 field to update',
+  })
 
 // Application inputs
 export type CreateTransactionInput = z.infer<typeof createTransactionSchema> &
@@ -29,6 +38,12 @@ export type CreateTransactionInput = z.infer<typeof createTransactionSchema> &
 
 export type UpdateTransactionInput = z.infer<typeof updateTransactionSchema> &
   Pick<Transaction, 'id' | 'userId'>
+
 export type FindTransactionByIdInput = Pick<Transaction, 'id' | 'userId'>
-export type FindAllTransactionsInput = Pick<Transaction, 'userId'>
+
+export type FindAllTransactionsInput = z.infer<
+  typeof transactionQueryParamsSchema
+> &
+  Pick<Transaction, 'userId'>
+
 export type DeleteTransactionInput = Pick<Transaction, 'id' | 'userId'>
