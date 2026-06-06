61253fb840db276685827e8e5d57d385a9c561f1
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-06-06 08:46:48 -0300

    refactor(telegram.dto): update telegram schemas to use positive integer for telegramId and restructure related schemas

--- src/modules/telegram/telegram.dto.ts
+++ src/modules/telegram/telegram.dto.ts
@@ -1,10 +1,33 @@
 import { z } from 'zod'
+import { categoryNameField } from '../categories/category.dto.js'
+import { transactionAmountInCentsField } from '../transactions/transaction.dto.js'
 import { emailField } from '../users/user.dto.js'
 
-export const telegramLinkAccountInput = z.object({
+const telegramIdField = z.int().positive() // group id are negative
+
+export const telegramBaseSchema = z.object({
   email: emailField,
   password: z.string().min(1),
-  telegramId: z.bigint(),
+  telegramId: telegramIdField,
 })
 
-export type TelegramLinkAccountInput = z.infer<typeof telegramLinkAccountInput>
+export const linkTelegramAccountSchema = telegramBaseSchema
+
+export const getUserIdByTelegramIdSchema = z.object({
+  telegramId: telegramIdField,
+})
+
+export const createNewTransactionFromTelegramSchema = z.object({
+  categoryName: categoryNameField,
+  amountInCents: transactionAmountInCentsField,
+})
+
+export type LinkTelegramAccountData = z.infer<typeof telegramBaseSchema>
+
+export type GetUserIdByTelegramIdData = z.infer<
+  typeof getUserIdByTelegramIdSchema
+>
+
+export type CreateNewTransactionFromTelegramData = z.infer<
+  typeof createNewTransactionFromTelegramSchema
+>
