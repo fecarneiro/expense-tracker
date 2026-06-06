91e1aeddeca1359295ab690a95aea36a2286bf84
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-06-06 08:48:03 -0300

    refactor(telegram.repository): change return type of linkAccount to Telegram and add findUserIdByTelegramId method for user retrieval

--- src/modules/telegram/telegram.repository.ts
+++ src/modules/telegram/telegram.repository.ts
@@ -1,21 +1,23 @@
+import { eq } from 'drizzle-orm'
 import { isUniqueViolation } from '../../database/db.error.js'
 import type { Database } from '../../database/db.js'
 import {
   type NewTelegram,
-  type PublicTelegram,
+  type Telegram,
   telegramTable,
 } from '../../database/schemas/telegram.schema.js'
+import type { GetUserIdByTelegramIdData } from './telegram.dto.js'
 import { TelegramAccountAlreadyExistsError } from './telegram.error.js'
 
 export class TelegramRepository {
   constructor(private readonly database: Database) {}
 
-  async linkAccount(data: NewTelegram): Promise<PublicTelegram | null> {
+  async linkAccount(data: NewTelegram): Promise<Telegram | null> {
     try {
       const [telegram] = await this.database
         .insert(telegramTable)
         .values(data)
-        .returning({ id: telegramTable.id, createdAt: telegramTable.createdAt })
+        .returning()
 
       return telegram ?? null
     } catch (err) {
@@ -24,5 +26,15 @@ export class TelegramRepository {
       }
       throw err
     }
+  }
+  async findUserIdByTelegramId(
+    data: GetUserIdByTelegramIdData,
+  ): Promise<Pick<Telegram, 'userId'> | null> {
+    const [user] = await this.database
+      .select({ userId: telegramTable.userId })
+      .from(telegramTable)
+      .where(eq(telegramTable.telegramId, data.telegramId))
+
+    return user ?? null
   }
 }
