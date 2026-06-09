c42f90f4ce3d70bdf22a2b2e248ad5f899441dd4
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-06-09 19:23:59 -0300

    refactor(telegram): reorganize BotContext type definition

--- src/modules/telegram/telegram.context.ts
+++ src/modules/telegram/telegram.context.ts
@@ -1,5 +1,12 @@
+import type { Conversation, ConversationFlavor } from '@grammyjs/conversations'
 import type { Context } from 'grammy'
 
-export type BotContext = Context & {
-  userId: string
-}
+export type BotContext = ConversationFlavor<
+  Context & {
+    userId: string
+  }
+>
+
+export type BotConversationContext = Context
+
+export type BotConversation = Conversation<BotContext, BotConversationContext>
