8d1a7f6de832c055b35f65cd707b295dc2f354a1
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-06-13 20:32:48 -0300

    refactor(database): update ssl configuration for production

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,13 +1,15 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
-import { env } from '../config/env.config.js'
+import { env, isProduction } from '../config/env.config.js'
 import { schemas } from './schemas/schema.js'
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 })
 
 export const db = drizzle({
