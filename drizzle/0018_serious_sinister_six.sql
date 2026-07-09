07a1ffc539d97a778821795ecf8282fca630f6e7
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-07-09 07:25:24 -0300

    chore(dependencies): update pnpm version to 11.9.0 and add new SQL migration for linking codes

---
+++ drizzle/0018_serious_sinister_six.sql
@@ -0,0 +1,5 @@
+CREATE TYPE "public"."purpose" AS ENUM('bot_link', 'user_link');--> statement-breakpoint
+ALTER TABLE "linking_codes" DROP CONSTRAINT "linking_codes_userId_unique";--> statement-breakpoint
+ALTER TABLE "linking_codes" ADD COLUMN "purpose" "purpose" DEFAULT 'bot_link' NOT NULL;--> statement-breakpoint
+ALTER TABLE "linking_codes" ALTER COLUMN "purpose" DROP DEFAULT;--> statement-breakpoint
+CREATE UNIQUE INDEX "linking_codes_unique_purpose_user_id" ON "linking_codes" USING btree ("purpose","user_id");
