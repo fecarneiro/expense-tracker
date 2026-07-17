cf5938f0bb7389685f1f6ce8a0eee3dc90f62d47
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-07-17 07:33:34 -0300

    refactor(http): remove unused API request module

--- apps/web/src/http.ts
+++
@@ -1,8 +0,0 @@
-const API_BASE_PATH = '/api'
-
-export async function apiRequest<T>(path: string, options: RequestInit = {}): Promise<T> {
-  const response = await fetch(`${API_BASE_PATH}${path}`, options)
-
-  // restante do tratamento...
-  return response.json() as Promise<T>
-}
